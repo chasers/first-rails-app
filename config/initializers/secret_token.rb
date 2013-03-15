f4ae6522ab7487ba430b103f149fe245d8911b91
Chase Granberry <chase@Chases-MacBook-Air.local>
2013-03-14 21:19:34 -0700

    chases first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChasesFirstRailsApp::Application.config.secret_token = '65096b4614cb333027bdef6ff9ae63229f921331d857a194743244980aa91cf2af58ba6a05cef34e244de85058cf98502ad420866a9bbd6aeb44baf351e51da5'
